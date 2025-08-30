d40c477a39af15675a6074feeabd482255808daf
Chengxuan Li <shine_lcx@outlook.com>
2025-08-30 10:50:56 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -6,6 +6,7 @@ import './style.css'
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
+  Layout: GlobalLayout,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
