b15e03e423997d02e62f24fb4d502d4b32c86a3e
Chengxuan Li <shine_lcx@outlook.com>
2025-08-30 10:53:43 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,17 +2,21 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+// 导入自定义的全局布局组件（关键缺失步骤）
+import GlobalLayout from './layouts/GlobalLayout.vue'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
+  // 使用导入的GlobalLayout作为布局组件
   Layout: GlobalLayout,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  // 如果你需要通过插槽
+  // Layout: () => {
+  //   return h(DefaultTheme.Layout, null, {
+  //     // 这里可以添加插槽内容，如之前的评论区配置
+  //   })
+  // },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    // 可以在这里注册全局组件或添加其他配置
   }
 }
